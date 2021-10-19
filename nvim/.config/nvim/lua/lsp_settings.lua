50a5866c573360c3302096da0998ac41a07e876e
Mark Baran <mark@gofastpay.com>
2021-10-19 11:57:21 -0700

    Disabled unused lsp servers

--- nvim/.config/nvim/lua/lsp_settings.lua
+++ nvim/.config/nvim/lua/lsp_settings.lua
@@ -30,7 +30,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Enable the following language servers
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'r_language_server' }
+local servers = { 'pyright', 'tsserver', 'r_language_server' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
