3e6187874ca3862eed7b97d3021c9c5d86eb9995
Mark Baran <mbaran@A0F01CB9214200C.localdomain>
2022-05-05 10:19:17 -0700

    QoL nvim-cmp edits

--- nvim/.config/nvim/lua/autocomplete_settings.lua
+++ nvim/.config/nvim/lua/autocomplete_settings.lua
@@ -7,10 +7,15 @@ local luasnip = require 'luasnip'
 -- nvim-cmp setup
 local cmp = require 'cmp'
 cmp.setup {
-    snippet = {expand = function(args) luasnip.lsp_expand(args.body) end},
+    completion = {
+        keyword_length = 3
+    }, 
+    snippet = {
+        expand = function(args) luasnip.lsp_expand(args.body) end
+    },
     mapping = {
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<Up>'] = cmp.mapping.select_prev_item(),
+        ['<Down>'] = cmp.mapping.select_next_item(),
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
@@ -38,5 +43,9 @@ cmp.setup {
             end
         end
     },
-    sources = {{name = 'nvim_lsp'}, {name = 'luasnip'}, {name = 'path'}}
+    sources = {
+        {name = 'nvim_lsp'},
+        {name = 'luasnip'},
+        {name = 'path'}
+    }
 }
