57239acfd9eda74683767dba6db41b4a9405aaa5
Mark Baran <mebaran@gmail.com>
2022-08-04 19:42:57 -0700

    Added better python support

--- nvim/.config/nvim/lua/plugins/null_ls_setup.lua
+++ nvim/.config/nvim/lua/plugins/null_ls_setup.lua
@@ -3,5 +3,9 @@ local null_ls = require("null-ls")
 null_ls.setup {
   sources = {
     null_ls.builtins.code_actions.gitsigns,
+    -- Python
+    null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.formatting.isort,
+    null_ls.builtins.formatting.autopep8,
   }
 }
