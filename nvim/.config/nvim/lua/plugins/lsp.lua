2aabae4d1cac704b085feb92466c16b784d68a0c
Mark Baran <mebaran@gmail.com>
2023-03-06 02:22:18 -0800

    Fixed up LspZeroFormat key binding

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,38 +1,41 @@
 local function lsp_zero_setup()
-    local lsp = require('lsp-zero').preset({
-        name = 'minimal',
+    local lsp = require("lsp-zero").preset({
+        name = "minimal",
         set_lsp_keymaps = true,
         manage_nvim_cmp = true,
         suggest_lsp_servers = false,
     })
+    lsp.on_attach(function(client, bufnr)
+        vim.keymap.set({ "n", "v" }, "<leader>=", "<cmd>LspZeroFormat<cr>", { noremap = true })
+    end)
 
     -- Snippet setup
     require("luasnip.loaders.from_vscode").lazy_load()
-    
+
     -- (Optional) Configure lua language server for neovim
     lsp.nvim_workspace()
     lsp.setup()
 end
 
 return {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v1.x",
     dependencies = {
         -- LSP Support
-        'neovim/nvim-lspconfig',             -- Required
+        "neovim/nvim-lspconfig",
 
         -- Autocompletion
-        'hrsh7th/nvim-cmp',         -- Required
-        'hrsh7th/cmp-nvim-lsp',     -- Required
-        'saadparwaiz1/cmp_luasnip', -- Optional
-        'hrsh7th/cmp-nvim-lua',     -- Optional
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp",
+        "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-nvim-lua",
 
         -- Snippets
         {
-            'L3MON4D3/LuaSnip',
-            build = 'make install_jsregexp'
+            "L3MON4D3/LuaSnip",
+            build = "make install_jsregexp",
         },
-        'rafamadriz/friendly-snippets' -- Optional
+        "rafamadriz/friendly-snippets",
     },
-    config = lsp_zero_setup
+    config = lsp_zero_setup,
 }
