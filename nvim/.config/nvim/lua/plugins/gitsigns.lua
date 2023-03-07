fcefcf42cf8d6cbdeefe768210ed413656e52319
Mark Baran <mbaran@avidxchange.com>
2023-03-07 15:46:11 -0800

    Added gitsigns desc to remind my addled brain of my keymap

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -22,18 +22,18 @@ local setupf = function ()
           return '<Ignore>'
         end, {expr=true})
 
-        -- Actions
-        map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-        map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-        map('n', '<leader>hS', gs.stage_buffer)
-        map('n', '<leader>hu', gs.undo_stage_hunk)
-        map('n', '<leader>hR', gs.reset_buffer)
-        map('n', '<leader>hp', gs.preview_hunk)
-        map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-        map('n', '<leader>tb', gs.toggle_current_line_blame)
-        map('n', '<leader>hd', gs.diffthis)
-        map('n', '<leader>hD', function() gs.diffthis('~') end)
-        map('n', '<leader>td', gs.toggle_deleted)
+            -- Actions
+            map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+            map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+            map('n', '<leader>hS', gs.stage_buffer, { desc = "Gitsigns stage buffer" })
+            map('n', '<leader>hu', gs.undo_stage_hunk, { desc = "Gitsigns undo stage hunk" })
+            map('n', '<leader>hR', gs.reset_buffer, { desc = "Gitsigns reset buffer" })
+            map('n', '<leader>hp', gs.preview_hunk, { desc = "Gitsigns preview hunk" })
+            map('n', '<leader>hb', function() gs.blame_line { full = true } end, { desc = "Gitsigns blame line" })
+            map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = "Gitsigns toggle current line blame" })
+            map('n', '<leader>hd', gs.diffthis, { desc = "Gitsigns diff this" })
+            map('n', '<leader>hD', function() gs.diffthis('~') end, { desc = "Gitsigns diff this ~" })
+            map('n', '<leader>td', gs.toggle_deleted, { desc = "Gitsigns toggle deleted" })
 
         -- Text object
         map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
