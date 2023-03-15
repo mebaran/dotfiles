94272d303b36acd8e65828099d1587989d5f2b8f
Mark Baran <mebaran@gmail.com>
2023-03-15 08:00:10 -0700

    Further keymap cleanup

--- nvim/.config/nvim/lua/keymap.lua
+++ nvim/.config/nvim/lua/keymap.lua
@@ -13,13 +13,13 @@ map("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Open Location List" })
 map("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Open Quickfix List" })
 
 -- Window resize (respecting `v:count`)
-map('n', '<C-Left>', '<Cmd>vertical resize -2<CR>', { desc = 'Decrease window width' })
-map('n', '<C-Down>', '<Cmd>resize -2<CR>', { desc = 'Decrease window height' })
-map('n', '<C-Up>', '<Cmd>resize +2<CR>', { desc = 'Increase window height' })
-map('n', '<C-Right>', '<Cmd>vertical resize +2<CR>', { desc = 'Increase window width' })
+map('n', '<C-Left>', '"<Cmd>vertical resize -" . v:count1 . "<CR>"', { expr = true, desc = 'Decrease window width' })
+map('n', '<C-Down>', '"<Cmd>resize -"          . v:count1 . "<CR>"', { expr = true, desc = 'Decrease window height' })
+map('n', '<C-Up>', '"<Cmd>resize +"          . v:count1 . "<CR>"', { expr = true, desc = 'Increase window height' })
+map('n', '<C-Right>', '"<Cmd>vertical resize +" . v:count1 . "<CR>"', { expr = true, desc = 'Increase window width' })
 
 -- Terminal
-map('t', '<C-w>', [[<C-\><C-n><C-w>]], { desc = "Quick window nav from terminal mode" })
+map('t', '<C-W>', [[<C-\><C-N><C-w>]], { desc = 'Focus other window' })
 map('t', '<C-n>', [[<C-\><C-n>]], { desc = "Quick access to normal mode" })
 map('n', '<leader>tv', "<cmd>vsplit | terminal <cr>", { desc = 'Split terminal vertically' })
 map('n', '<leader>tx', "<cmd>split | resize 20 | terminal<cr>", { desc = 'Split terminal horizontally' })
