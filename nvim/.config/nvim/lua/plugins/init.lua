fccfb66696bea5c7a7f6652e451d87fe3ca28a04
Mark Baran <mebaran@gmail.com>
2023-04-03 11:05:29 -0700

    Added which-key to config

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -7,21 +7,29 @@ return {
     },
     'nvim-treesitter/nvim-treesitter-textobjects',
     {
-	    'tversteeg/registers.nvim',
-	    name = "registers",
-	    keys = {
-		    { "\"",    mode = { "n", "v" } },
-		    { "<C-R>", mode = "i" }
-	    },
-	    cmd = "Registers",
+        'tversteeg/registers.nvim',
+        name = "registers",
+        keys = {
+            { "\"",    mode = { "n", "v" } },
+            { "<C-R>", mode = "i" }
+        },
+        cmd = "Registers",
         config = true
     },
     {
         'numtostr/comment.nvim',
         keys = {
-            { "gc", mode = {'n', 'v'} },
-            { "gb", mode = {'n', 'v'} }
+            { "gc", mode = { 'n', 'v' } },
+            { "gb", mode = { 'n', 'v' } }
         },
         config = true
     },
+    {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup({})
+        end,
+    }
 }
